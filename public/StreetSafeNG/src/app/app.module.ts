b7672be43505b90a17854fd00d99198a772dfa7f
Chuck <cfunkles@gmail.com>
2017-09-23 13:18:35 -0600

    add core modules for use add routing module

--- public/StreetSafeNG/src/app/app.module.ts
+++ public/StreetSafeNG/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import { HttpModule } from '@angular/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './mainApp/app.component';
+import { HomeComponent } from './homecomponent/home.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
